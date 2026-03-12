25f48d67397c3f6f36add7ba9ac7c28a58683a06
Abhisheklatoriya <113186319+Abhisheklatoriya@users.noreply.github.com>
2026-03-12 12:25:55 -0400

    Update page title from 'Badger Tools' to 'Badger Workflows'

--- Main_App.py
+++ Main_App.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 
 # --- 1. Global Page Config ---
-st.set_page_config(page_title="Badger Tools", page_icon="🦡", layout="wide")
+st.set_page_config(page_title="Badger Workflows", page_icon="🦡", layout="wide")
 
 # Custom CSS for a clean, professional look and "App Cards"
 st.markdown("""
